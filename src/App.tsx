3579116af5b52869838731e203f061804f50097f
Matheus Souza <matheus.leandro-terceiro@cea.com.br>
2022-06-22 12:55:07 -0300

    adding apollo and graphql

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`;
+
+interface Lesson {
+  id: string;
+  title: string;
+}
+
 function App() {
+  const { data } = useQuery<{ lessons: Lesson[]; }>(GET_LESSONS_QUERY);
+
   return (
-    <h1 className="text-5xl font-bold text-violet-500">Hello Word</h1>
+    <ul>
+      {data?.lessons.map(lesson => {
+        return <li key={lesson.id}>{lesson.title}</li>;
+      })}
+    </ul>
   );
 }
 
