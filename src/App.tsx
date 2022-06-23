6d3fdfc3d371573ff967f6ba88d81fce88b408b0
Matheus Souza <matheus.leandro-terceiro@cea.com.br>
2022-06-23 12:22:46 -0300

    applying routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import { Event } from "./pages/Event";
+import { ApolloProvider } from '@apollo/client';
+import { Router } from "./Router";
+import { client } from './lib/apollo';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Event />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   );
 }
 
