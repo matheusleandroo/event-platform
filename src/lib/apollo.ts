0fddede0e68545399aca4fe6fa831a1f3e29d450
Matheus Souza <matheus.leandro-terceiro@cea.com.br>
2022-06-23 12:49:25 -0300

    adding env

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,8 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
+const apiURL = import.meta.env.VITE_API_URL;
+
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.graphcms.com/v2/cl4pqrn7q2e1901xlblj2btx5/master',
+  uri: apiURL,
   cache: new InMemoryCache()
 });
