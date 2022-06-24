6795f22117fc95716c48c0753ceb27156c2d8518
Matheus Souza <matheus.leandro-terceiro@cea.com.br>
2022-06-24 12:24:17 -0300

    adding apiAccessToken

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,8 +1,12 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const apiURL = import.meta.env.VITE_API_URL;
+const apiAccessToken = import.meta.env.VITE_API_ACCESS_TOKEN;
 
 export const client = new ApolloClient({
   uri: apiURL,
+  headers: {
+    'Authorization': `Bearer ${apiAccessToken}`
+  },
   cache: new InMemoryCache()
 });
